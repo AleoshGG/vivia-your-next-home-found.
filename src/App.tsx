f605e4b981e5824b4df05ce4d1f5e5d1a7cde7bb
Alexis Guzmán <spartanthealexvro@gmail.com>
2026-05-18 13:45:57 -0600

    Guardando dominio

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/vivia-your-next-home-found./">
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
